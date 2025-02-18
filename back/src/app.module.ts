251ec5b229cef9ab0b8ff97de9a08c9d3c9be21b
Sofia Agüero <sofiaguero3312@gmail.com>
2025-02-18 17:50:48 -0300

    modulos principales

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthService } from './auth/auth.service';
+import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
+import { AppointmentModule } from './appointment/appointment.module';
+import { MembershipModule } from './membership/membership.module';
+import { PaymentModule } from './payment/payment.module';
+import { RoutineModule } from './routine/routine.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule, AuthModule, AppointmentModule, MembershipModule, PaymentModule, RoutineModule],
+  controllers: [AppController, AuthController],
+  providers: [AppService, AuthService],
 })
 export class AppModule {}
