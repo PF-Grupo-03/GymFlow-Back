779c0fca30ac77f34f0e7cc9fabbff62b0f2707d
Sofia Agüero <sofiaguero3312@gmail.com>
2025-02-20 21:21:13 -0300

    Archivo main y middlewares

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('GymFlow Backend')
+    .setDescription('API for GymFlow Backend')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+
+  SwaggerModule.setup('api', app, document);
+
+  const loggerMiddleware = new LoggerMiddleware();
+  app.use(loggerMiddleware.use);
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
