1bf532674095c6dfe5c0bac77f33bedc889e1d19
Sofia Agüero <sofiaguero3312@gmail.com>
2025-02-24 18:01:23 -0300

    Users y roles

--- back/src/users/users.service.ts
+++ back/src/users/users.service.ts
@@ -1,18 +1,27 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
+import { CreateUserDto } from './dtos/users.dto';
+import { ApproveTrainerDto } from './dtos/approveTrainer.dto';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly prisma: PrismaService) {}
 
-  private excludePassword(user: any) {
+  private excludePassword(user: CreateUserDto) {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const { password, ...userWithoutPassword } = user;
     return userWithoutPassword;
   }
 
   async getAllUsers() {
-    const users = await this.prisma.users.findMany();
+    const users = await this.prisma.users.findMany({
+      include: { member: true },
+    });
 
     if (!users.length) throw new NotFoundException('Usuarios no encontrados');
 
@@ -24,24 +33,53 @@ export class UsersService {
       where: {
         id: id,
       },
+      include: { member: true },
     });
 
     if (!user) throw new NotFoundException('Usuario no encontrado');
     return this.excludePassword(user);
   }
 
-  async updateUser(id: string, updatedData: any) {
-    const user = await this.prisma.users.update({
-      where: { id },
-      data: updatedData,
-    });
-    if (!user) throw new NotFoundException(`el usuario de id ${id} no existe`);
-    return this.excludePassword(user);
+  async updateUser(id: string, updatedData: Partial<CreateUserDto>) {
+    try {
+      const user = await this.prisma.users.update({
+        where: { id },
+        data: updatedData,
+        include: { member: true },
+      });
+
+      return this.excludePassword(user);
+    } catch (error) {
+      if (error.code === 'P2025') {
+        throw new NotFoundException(`El usuario con ID ${id} no existe`);
+      }
+      throw new InternalServerErrorException('Error al actualizar el usuario');
+    }
   }
 
   async findUserByEmail(email: string) {
     return await this.prisma.users.findUnique({
       where: { email },
+      include: { member: true },
+    });
+  }
+
+  async approveTrainer(userId: string, dto: ApproveTrainerDto) {
+    const user = await this.prisma.user.findUnique({ where: { id: userId } });
+
+    if (!user) {
+      throw new NotFoundException('Usuario no encontrado');
+    }
+
+    if (user.UserRole !== 'USER_TRAINING') {
+      throw new BadRequestException(
+        'Solo los entrenadores pueden ser aprobados',
+      );
+    }
+
+    return this.prisma.user.update({
+      where: { id: userId },
+      data: { approved: dto.approved },
     });
   }
 }
