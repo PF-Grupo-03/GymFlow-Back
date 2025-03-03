2f2eae9196da69c2c42c6f6a228556accac38d9b
PF-Grupo-03 <proyecto.final.g03@gmail.com>
2025-03-03 13:47:33 -0300

    logica de auth google y su dto

---
+++ back/src/users/dtos/updateUser.dto.ts
@@ -0,0 +1,31 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsOptional, IsString, IsBoolean, Matches, Length } from 'class-validator';
+
+export class UpdateUserDto {
+    @ApiProperty({example: '12345678'})
+    @IsOptional()
+    @IsString()
+    @Matches(/^\d{7,8}$/, {
+        message: 'El DNI debe contener entre 7 y 8 dígitos numéricos',
+      })
+    dni?: string;
+
+    @ApiProperty({example: 'Calle 123'})
+    @IsOptional()
+    @IsString()
+    @Length(3, 80)
+    address?: string;
+
+    @ApiProperty({example: '+541112345678'})
+    @IsOptional()
+    @IsString()
+    @Matches(/^\+?\d{7,15}$/, {
+        message:
+          'El teléfono debe contener solo números y puede incluir un "+" al inicio',
+      })
+    phone?: string;
+
+    @IsOptional()
+    @IsBoolean()
+    approved?: boolean;
+}
