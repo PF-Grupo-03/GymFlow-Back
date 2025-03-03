0693c62480bb0048890d1affbc9e3ba1e0c26467
RLopezFernandez <rodrigolopez010@gmail.com>
2025-03-03 16:25:14 -0300

    Nuevas modificaciones de Payment

--- back/src/payment/payment.module.ts
+++ back/src/payment/payment.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PaymentController } from './payment.controller';
 import { PaymentService } from './payment.service';
+import { MercadoPagoService } from './Mp/mercadoPago';
+import { UsersService } from 'src/users/users.service';
 
 @Module({
   controllers: [PaymentController],
-  providers: [PaymentService]
+  providers: [PaymentService, MercadoPagoService, UsersService],
 })
 export class PaymentModule {}
